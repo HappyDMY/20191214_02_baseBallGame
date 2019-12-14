b42c567fce6e223f77b0ba211e46a3ce421d53bd
jun <vagrantsj@gmail.com>
2019-12-14 14:15:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20191214_02_baseballgame/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.example.a20191214_02_baseballgame
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    val mathContext = this
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
